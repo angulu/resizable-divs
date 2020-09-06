aff25433f1deb206d979fe0e602e234edeee0c32
Angel Ngulumbi <angulumbi@middlebury.edu>
2020-09-06 10:46:15 -0400

    Updates

---
+++ precache-manifest.5367fe8493920e4a7735803fb399bfcf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6001f8a6f3a47fb22c606b74544c2a97",
+    "url": "/resizable-divs/index.html"
+  },
+  {
+    "revision": "1ef41a0cd0f594cb6dfe",
+    "url": "/resizable-divs/static/css/main.efea1d2a.chunk.css"
+  },
+  {
+    "revision": "67e552d24d33cb57dfd0",
+    "url": "/resizable-divs/static/js/2.ef21704b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resizable-divs/static/js/2.ef21704b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ef41a0cd0f594cb6dfe",
+    "url": "/resizable-divs/static/js/main.d289c9e7.chunk.js"
+  },
+  {
+    "revision": "0081fe65e7eaeabea535",
+    "url": "/resizable-divs/static/js/runtime-main.29568d6f.js"
+  }
+]);
