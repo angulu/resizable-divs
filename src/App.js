95fb506a8315e4ed2b265af3be7d058e92da2b16
Angel Ngulumbi <angulumbi@middlebury.edu>
2020-09-06 10:37:07 -0400

    callenge

--- src/App.js
+++ src/App.js
@@ -1,26 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Resize from './Resize';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor () {
+    super ()
+
+    this.state = {
+      columns: [],
+      colors: ["red", "orange", "yellow", "green", "blue", "purple", "indigo"],
+      total_columns: 0,
+      maximize: null
+    };
+
+  }
+
+  addColumn = () => {
+
+    if (this.state.columns.length > 0) {
+
+        let num = this.state.total_columns + 1;
+
+        this.setState(prevState => ({
+          columns: prevState.columns.concat("C" + num),
+          total_columns: num
+        }))
+
+      
+
+    } else {
+
+      this.setState(prevState => ({
+        columns: prevState.columns.concat("C0")
+      }))
+
+    }
+
+  }
+
+  deleteColumn = (name) => {
+
+    console.log(name);
+
+    this.setState(prevState => ({
+      columns: prevState.columns.filter( item => item !== name )
+    }))
+
+  }
+
+  render() {
+
+    const { colors, columns, maximize } = this.state;
+
+    return (
+
+      <div>
+        <button className="button right" onClick={this.addColumn}>Add Column</button>
+        
+        {columns && <Resize total = {columns.length} max = {maximize}>
+
+          {
+            columns.map((item, index) => 
+              <div
+                key={index}
+                style={{backgroundColor: `${colors[index % 7]}`}}
+                className="column"
+                onDoubleClick={ () => this.setState({ maximize: index })}
+              >
+                <label>{item}</label>
+                <small 
+                  style ={{cursor: 'pointer'}}
+                  onClick={this.deleteColumn.bind(this, item)}>x</small>
+              </div>
+            )
+          }
+          
+        </Resize> }
+      </div>
+
+    )
+  }
+
 }
 
 export default App;
---
+++ src/Resize.js
@@ -0,0 +1,202 @@
+import React, { Component } from 'react';
+
+class Resize extends Component {
+    eventHandler = null
+  
+    constructor (props) {
+      super(props)
+      
+      this.state = {
+        isDragging: false,
+        total_panels: this.props.total,
+        panels: []
+      }
+    }
+  
+    componentDidMount () {
+      document.addEventListener('mousemove', this.resizePanel)
+      document.addEventListener('mouseup', this.stopResize)
+      document.addEventListener('mouseleave', this.stopResize)
+    }
+
+    componentDidUpdate (prevProps) {
+        if (prevProps.total !== this.props.total){
+            this.widthCalc(this.props.total)
+        }
+
+        if (prevProps.max !== this.props.max){
+            this.maxCalc(this.props.max, this.props.total)
+        }
+    }
+
+    startResize = (e, index) => {
+      this.setState({
+        isDragging: true,
+        currentPanel: index,
+        adjacentPanel: index - 1,
+        initialPos: e.clientX
+      })
+
+    }
+    
+    stopResize = () => {
+      if (this.state.isDragging) {
+
+        this.setState({
+          isDragging: false,
+          delta: 0,
+          previous_delta: 0,
+          currentPanel: null
+        })
+
+        console.log("stop", this.state.currentPanel, this.state.delta);
+
+      }
+    }
+    
+    resizePanel = (event) => {
+
+      if (this.state.isDragging) {
+
+        const {adjacentPanel, currentPanel, previous_delta, panels, initialPos} = this.state;
+        
+        let temp = panels;
+
+        let temp_0 = temp[currentPanel]
+        let temp_1 = temp[adjacentPanel]
+
+        let delta = event.clientX - initialPos
+
+        let previous = previous_delta ? previous_delta : delta
+
+        console.log("X", event.clientX, temp_0, temp_1);
+
+        // if column is extending right columns (when delta is positive)
+        if ( delta > 0 ) {
+
+            if (temp_0 <= 0) {
+
+                temp_0 = 0
+
+                if (temp.length > currentPanel + 1) {
+                    console.log("hehe", temp.length, currentPanel + 1);
+
+                    this.setState((prevState) => ({
+                        currentPanel: prevState.currentPanel++
+                    }))
+
+                } else {
+                    temp_1 += temp_0
+                    console.log("EXCEEEDING hehe");
+                }
+
+            } else {
+                temp_1 += (delta - previous)
+                temp_0 -= (delta - previous);
+            }
+           
+        } else if ( delta < 0 ) {
+
+            if (temp_1 <= 0) {
+
+                temp_1 = 0
+
+                if (currentPanel - 1 !== 0) {
+                    console.log("hihi", currentPanel - 1);
+
+                    this.setState((prevState) => ({
+                        adjacentPanel: prevState.adjacentPanel--
+                    }))
+
+                }
+                else {
+                    temp_0 += temp_1
+                    console.log("EXCEEEDING hihi");
+                }
+
+            } else {
+
+                temp_1 += (delta - previous)
+                temp_0 -= (delta - previous);
+            }
+           
+        }
+
+        temp.splice(currentPanel, 1, temp_0)
+        temp.splice(adjacentPanel, 1, temp_1)
+
+        console.log("panels", temp);
+
+        this.setState({
+          panels: temp,
+          previous_delta: delta
+        })
+
+      }
+
+    }
+
+    widthCalc = (number) => {
+
+        let temp = [], num = (window.innerWidth - ((number - 1) * 10)) / number  // 10 is order plus resizer width
+
+        for (let i = 0; i < number; i++) {
+            temp.push(num)            
+        }
+        
+        this.setState({ panels: temp })
+    }
+
+    maxCalc = (index, number) => {
+
+        let temp = [], num = (number - 1) * 8;
+
+        for (let i = 0; i < number; i++) {
+
+            if (i !== index) {
+                temp.push(0)
+            } else {
+                temp.push(window.innerWidth - num)
+            }
+                        
+        }
+        
+        this.setState({ panels: temp })
+    }
+    
+    render() {
+
+      const { panels, delta, currentPanel } = this.state;
+
+      const rest = this.props.children;
+
+      return (
+        <div className="panel-container" onMouseUp={() => this.stopResize()}>
+         
+          {[].concat(...rest.map((child, i) => {
+              if ( i === 0 ) {
+                  return [
+                    <div key={"panel_" + i} className="panel" style={{minWidth: panels[i] + "px"}}>
+                        {child}
+                    </div>
+                  ]
+              } else {
+                return [
+                    <div onMouseDown={(e) => this.startResize(e, i)}
+                        key={"resizer_" + i}
+                        className="resizer">
+                    </div>,
+                    <div key={"panel_" + i} className="panel" style={{minWidth: panels[i] + "px"}}>
+                        {child}
+                    </div>
+                  ]
+              }
+            
+          }))}
+        </div>
+      )
+    }
+}
+  
+
+export default Resize;
